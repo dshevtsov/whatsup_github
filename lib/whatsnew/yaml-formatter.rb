18ebbcced003220322695f0b0c03ca9c365ba403
Dima Shevtsov <shevtsov@adobe.com>
2019-01-23 12:45:37 -0600

    Added 'updated' to the output

--- lib/whatsnew/yaml-formatter.rb
+++ lib/whatsnew/yaml-formatter.rb
@@ -2,21 +2,28 @@ require 'yaml'
 module Whatsnew
   # Table containing Rows
   class YAMLFormatter
-
     # def initialize(since)
     #   @collector = RowCollector.new(since: since)
     # end
 
-    def generate_output_from content
-      content.collect do |object|
+    def generate_output_from(content)
+      entries =
+        content.collect do |object|
+          {
+            'description' => object.description,
+            'versions' => object.versions,
+            'type' =>  object.type,
+            'date' => object.date,
+            'link' => object.link
+          }
+
+        end
+      output =
         {
-          'description' => object.description,
-          'versions' => object.versions,
-          'type' =>  object.type,
-          'date' => object.date,
-          'link' => object.link
+          'updated' => Time.now.strftime('%c'),
+          'entries' => entries
         }
-      end.to_yaml
+      output.to_yaml
     end
   end
 end

5d4cc4d30fb4ef8cadb24fea410c91cb14928eec
Dmytro Shevtsov <shevtsov@adobe.com>
2023-01-05 11:11:25 -0600

    Add a CLI option --config for config filename

--- lib/whatsup_github/cli.rb
+++ lib/whatsup_github/cli.rb
@@ -7,7 +7,11 @@ module WhatsupGithub
   # CLI options
   class CLI < Thor
     desc 'since DATE', 'Filters pull requests since the specified date till now. Default: last 7 days.'
+    option :config,
+           desc: 'Relative path to the configuration file.',
+           default: '.whatsup.yml'
     def since(date = Date.today - 7)
+      WhatsupGithub::Config.filename = options[:config]
       runner = WhatsupGithub::Runner.new(Date.parse(date.to_s))
       runner.run
     end
